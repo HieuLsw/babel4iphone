e6f9b69627c0cc4dfe46247c9339b365c32a8f9b
amatig <amatig@5ab93b3a-df7a-11de-8d18-5b59c22968bc>
2010-01-25 16:31:22 +0000

    ok

--- trunk/Net/server.py
+++ trunk/Net/server.py
@@ -1,7 +1,9 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
+import threading
 
+# il socket riceve solo gli eventi e li indirizza al factory x attivare i comandi
 class ServerProtocol(LineReceiver):
     
     def __init__(self):
@@ -23,34 +25,46 @@ class ServerProtocol(LineReceiver):
         self.factory.delClient(self.username)
 
 
+# tutte le operazioni devono essere fatte nel factory e gestite con i lock
 class ServerFactory(Factory):
     protocol = ServerProtocol
+    clients_lock = threading.Lock()
     
     def __init__(self):
+        self.clients_lock.acquire()
         self.clients = {}
+        self.clients_lock.release()
     
     def addClient(self, username, client):
+        self.clients_lock.acquire()
         if not self.clients.has_key(username):
             client.username = username
             self.clients[username] = client
             print "Add user %s." % username
         else:
             print "User named %s exists."
+        self.clients_lock.release()
     
     def delClient(self, username):
+        self.clients_lock.acquire()
         if self.clients.has_key(username):
             del self.clients[username]
             print "delete %s." % username
+        self.clients_lock.release()
     
     def sendAll(self, message):
+        self.clients_lock.acquire()
         for username, client in self.clients.items():
             client.sendLine(message)
+        self.clients_lock.release()
     
     def send(self, username, message):
+        self.clients_lock.acquire()
         if self.clients.has_key(username):
             self.clients[username].sendLine(message)
         else:
-            print "No user named %s." % username
+            print "No send msg, username %s not exist." % username
+        self.clients_lock.release()
 
 
 def loop(f):
